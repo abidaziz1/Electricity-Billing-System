ef55b5566a0c8e4336d024094e25bd3ea040cc6d
Abid Aziz <139110271+abidaziz1@users.noreply.github.com>
2024-08-30 22:17:21 +0600

    Update Splash.java

--- src/electricity/billing/system/Splash.java
+++ src/electricity/billing/system/Splash.java
@@ -11,7 +11,7 @@ public class Splash extends JFrame {
 
         ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon/splash/Splash.jpg"));
         Image imageOne = imageIcon.getImage().getScaledInstance(600,400,Image.SCALE_DEFAULT);
-        ImageIcon imageIcon2 = new ImageIcon( imageOne);
+        ImageIcon imageIcon2 = new ImageIcon(imageOne);
         JLabel imageLabel = new JLabel(imageIcon2);
         add(imageLabel);
 
